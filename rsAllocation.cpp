dd12378b6064cf736c5a78cca4cb3c445b5796ac
Yong Chen <yong.a.chen@intel.com>
2013-10-11 21:05:10 +0800

    [PORT FROM ABT] Add RS allocation destroy functions
    BZ: 143436
    
    Define AllocationDestroy in rs.spec, add destroy functions in libRS so
    we can invoke rs hal destroy function to cleanup allocation resource.
    
    Change-Id: I904aae7432ca0e5aa8290db42e15272cf6cd94cc
    Category: aosp improvement
    Domain: AOSP-RenderScript
    Origin: internal
    Upstream-Candidate: yes
    Orig-ABT-Change-Id: Id7dd630719b21dddf1ad80380482eccacd33e370
    Signed-off-by: Yong Chen <yong.a.chen@intel.com>

--- rsAllocation.cpp
+++ rsAllocation.cpp
@@ -513,6 +513,10 @@ void Allocation::ioReceive(const Context *rsc) {
 #endif
 }
 
+void Allocation::destroy(const Context *rsc) {
+    freeChildrenUnlocked();
+    rsc->mHal.funcs.allocation.destroy(rsc, this);
+}
 
 /////////////////
 //
@@ -714,6 +718,11 @@ void rsi_AllocationIoSend(Context *rsc, RsAllocation valloc) {
 void rsi_AllocationIoReceive(Context *rsc, RsAllocation valloc) {
     Allocation *alloc = static_cast<Allocation *>(valloc);
     alloc->ioReceive(rsc);
+}
+
+void rsi_AllocationDestroy(Context *rsc, RsAllocation valloc) {
+    Allocation *alloc = static_cast<Allocation *>(valloc);
+    alloc->destroy(rsc);
 }
 
 void rsi_Allocation1DRead(Context *rsc, RsAllocation va, uint32_t xoff, uint32_t lod,
